1245297a7e6a7bdd4cb785b663cbb5158cfdf436
Souhaib Moustajib <souhaibmoustajib@gmail.com>
2026-07-16 15:01:13 +0100

    starting with the simulation engine

--- src/engine.py
+++ src/engine.py
@@ -1,5 +1,44 @@
-from .parsing import FlightNetworkParser, FlightNetwork
-import sys
+from .parsing import FlightNetwork
+from enum import Enum
+from .graph import Graph
+
+
+class DroneStatus(Enum):
+    WAITING = "waiting"
+    MOVING = "moving"
+    IN_TRANSIT = "in_transit"
+    DELIVERED = "delivered"
+
+
+class Drone:
+    def __init__(self, drone_id: int, start_zone: str):
+        self.id = drone_id
+        self.status = DroneStatus.WAITING
+        self.location = start_zone
+        self.destination = None
+        self.arrival_turn = None
+
+    def __repr__(self):
+        return f"Drone(id={self.id}, status={self.status.value}," \
+            f" location={self.location})"
+
+
+class SimulationState:
+    def __init__(self, g: Graph):
+        self.graph = g
+        self.turn = 0
+
+        start_name = g.network.start_hub.name
+        self.drones = [
+            Drone(drone_id=i, start_zone=start_name)
+            for i in range(1, g.network.nb_drones + 1)
+        ]
+
+        self.zone_occupancy = {}
+        self.zone_occupancy[start_name] = g.network.nb_drones
+        for zone in g.graph:
+            if zone != start_name:
+                self.zone_occupancy[zone] = 0
 
 
 class Engine:
@@ -12,27 +51,3 @@ class Engine:
 
     def update_drones(self):
         pass
-
-
-class Drone:
-    def __init__(self, id: int, zone: str, network: FlightNetwork):
-        self.id = id
-        self.zone = zone
-        self.network = network
-
-    def move(self):
-        pass
-
-    def next_zone(self):
-        pass
-
-
-def main() -> None:
-    if len(sys.argv) != 2:
-        print("Usage: python3 -m src <map_file>")
-        sys.exit(1)
-    network = FlightNetworkParser.parse_file(sys.argv[1])
-    engine = Engine(network)        
-
-if __name__ == '__main__':
-    main()
